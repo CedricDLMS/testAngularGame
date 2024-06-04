c4205e06dffdbd4c7a085d767a7a5524157c1ac4
CedricDLMS <delmascede@gmail.com>
2024-06-04 17:17:13 +0200

    class creation

---
+++ src/app/Features/player/Model/player.model.ts
@@ -0,0 +1,8 @@
+export default class Player{
+    xPosition : number;
+    yPosition : number;
+    constructor(xPosition : number, yPosition : number){
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+    }
+}
